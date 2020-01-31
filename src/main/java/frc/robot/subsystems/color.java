0fb60d21e04db73afa39536d82ddbf2c30aa72c0
cgannaway <ccgannaway@me.com>
2020-01-31 12:13:44 -0600

    comments and test

--- src/main/java/frc/robot/subsystems/color.java
+++ src/main/java/frc/robot/subsystems/color.java
@@ -7,18 +7,20 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.ColorSensorV3;
 import com.revrobotics.ColorMatch;
 import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorSensorV3;
 
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class color extends SubsystemBase {
-
+  //declare color sensor
   ColorSensorV3 m_ColorSensorV3 = new ColorSensorV3(Constants.i2cport);
+  //declare color matcher
   ColorMatch m_colorMatcher = new ColorMatch();
+  //declare color values
   final Color BlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
   final Color GreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
   final Color RedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
@@ -27,19 +29,19 @@ public class color extends SubsystemBase {
    * Creates a new color.
    */
   public color() {
-  
-
+    //add color values to the matcher
     m_colorMatcher.addColorMatch(BlueTarget);
     m_colorMatcher.addColorMatch(GreenTarget);
     m_colorMatcher.addColorMatch(RedTarget);
     m_colorMatcher.addColorMatch(YellowTarget);
   }
 
-  
+
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    //get detected color + match to a color
     Color detected = m_ColorSensorV3.getColor();
     ColorMatchResult c = m_colorMatcher.matchClosestColor(detected);
     if(c.color==BlueTarget){
